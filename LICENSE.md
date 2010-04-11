84628e3a6aa7a25f7a1567149259eaf5067f75f3
Brian M. Clapper <bmc@clapper.org>
2010-04-10 21:18:24 -0400

    Handle 64-bit test machines

--- LICENSE.md
+++ LICENSE.md
@@ -1,7 +1,12 @@
+---
+title: License for Java EditLine
+layout: default
+---
+
 This software is released under a BSD license, adapted from
 <http://opensource.org/licenses/bsd-license.php>
 
-Copyright (c) 2008-2010 Brian M. Clapper.
+Copyright &copy; 2010 Brian M. Clapper.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
--- models.py
+++ models.py
@@ -3,7 +3,10 @@ import sys
 
 from google.appengine.ext import db
 
-FETCH_THEM_ALL = sys.maxint - 1
+# If the local platform is 64 bit, just using sys.maxint can cause problems.
+# It will evaluate to a number that's too large for GAE's 32-bit environment.
+# So, force it to a 32-bit number.
+FETCH_THEM_ALL = ((sys.maxint - 1) >> 32) & 0xffffffff
 
 class Article(db.Model):
 
